ef763022f552350a8d62b9d87e6ad83b8f95365b
SrG640 <sergifloresvaro@elpuig.xeill.net>
2026-03-17 17:28:10 +0100

    mapsb0.4

---
+++ Assets/Maps/SpriteTowerTile.tsx
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="TileSet" tilewidth="32" tileheight="32" tilecount="36" columns="12">
+ <image source="TileSet.png" width="384" height="96"/>
+ <tile id="0">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup>
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="0" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup>
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup>
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup>
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
