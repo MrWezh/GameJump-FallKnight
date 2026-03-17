859fb78b9a4421e4436d3b9127045f1a13c31780
SrG640 <sergifloresvaro@elpuig.xeill.net>
2026-03-17 16:51:41 +0100

    Error map fixed

---
+++ Assets/Maps/SpriteTowerTile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="TileSet" tilewidth="32" tileheight="32" tilecount="36" columns="12">
+ <image source="TileSet.png" width="384" height="96"/>
+</tileset>
